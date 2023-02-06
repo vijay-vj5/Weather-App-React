22843f5a655c9985cc3ec3d6849cda5c4be0ff3c
vijay-vj5 <105633955+vijay-vj5@users.noreply.github.com>
2023-02-06 18:01:00 +0530

    Add files via upload

---
+++ src/Credits.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Credits.css"
+
+export default function Credits() {
+    return (
+      <div className="Credits">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h6 className="credits-section">
+                {" "}
+                Coded by vijay,{" "}
+                <a
+                  className="link-github"
+                  href="https://github.com/milicodes/weather-app-react"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  open sourced on Github.
+                </a>
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
